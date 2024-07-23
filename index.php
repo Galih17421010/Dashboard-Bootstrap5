11307055fa9d08d4d2f524d3bb2d2f111bd19a2f
Tiyasasih25 <tiyasasih532@gmail.com>
2024-07-23 11:16:33 +0700

    Add Index

--- index.php
+++ index.php
@@ -5,20 +5,6 @@
     <!-- Required meta tags -->
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-    <meta name="author" content="Codescandy" />
-
-    <!-- Google tag (gtag.js) -->
-    <!-- <script async src="https://www.googletagmanager.com/gtag/js?id=G-M8S4MT3EYG"></script> -->
-    <!-- <script>
-        window.dataLayer = window.dataLayer || [];
-        function gtag() {
-            dataLayer.push(arguments);
-        }
-        gtag('js', new Date());
-
-        gtag('config', 'G-M8S4MT3EYG');
-    </script> -->
-
     <!-- Favicon icon-->
     <link rel="shortcut icon" type="image/x-icon" href="./assets/images/favicon.ico" />
 
